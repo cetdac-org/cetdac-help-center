60f7c4562de931f80693a3093ce497d8cf11bc9d
AlanGuo <AlanGuo>
2018-09-22 11:02:57 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,7 +6,7 @@ module.exports = {
       lang: 'en-US', // 将会被设置为 <html> 的 lang 属性
       title: 'BITAPP Developer Document',
       description: 'BitApp, Create your life with blockchain',
-      keywords: 'eth,以太坊,bch,bcc,bitcoin cash,比特现金,sdk,bitapp,开发者,水龙头,测试币,测试币领取,免费,free,ropsten,testnet,faucet,多币种,支付按钮,跨链开发,区块链开发,智能合约,比特币,btc',
+      keywords: 'eth,以太坊,bch,bcc,bitcoin cash,比特现金,sdk,bitapp,开发者,水龙头,测试币,测试币领取,免费,free,ropsten,testnet,faucet,多币种,支付按钮,跨链开发,区块链开发,智能合约,比特币,btc,钱包,wallet,extension,插件钱包',
       head: [
         ['link', { rel: 'icon', href: '/favicon.ico' }]
       ]
@@ -15,7 +15,7 @@ module.exports = {
       lang: 'zh',
       title: '开发者文档 | BITAPP',
       description: 'BitApp, 区块链开发第一站',
-      keywords: 'eth,以太坊,bch,bcc,bitcoin cash,比特现金,sdk,bitapp,开发者,水龙头,测试币,测试币领取,免费,free,ropsten,testnet,faucet,多币种,支付按钮,跨链开发,区块链开发,智能合约,比特币,btc',
+      keywords: 'eth,以太坊,bch,bcc,bitcoin cash,比特现金,sdk,bitapp,开发者,水龙头,测试币,测试币领取,免费,free,ropsten,testnet,faucet,多币种,支付按钮,跨链开发,区块链开发,智能合约,比特币,btc,钱包,wallet,extension,插件钱包',
       head: [
         ['link', { rel: 'icon', href: '/favicon.ico' }]
       ]
